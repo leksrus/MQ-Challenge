17fb3ded7ec8fbb62d90973fa024eba9e4b8c1a0
Aleksey Kotylev <aleks_rus@hotmail.com>
2020-01-13 18:11:42 -0300

    Moving file creator to separate class

---
+++ Api.Services/FileManager.cs
@@ -0,0 +1,53 @@
+﻿using Api.Entitys;
+using Api.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using System;
+using System.IO;
+
+namespace Api.Services
+{
+    public class FileManager : IFileManager
+    {
+        private readonly IOptions<MQConfig> _options;
+
+        public FileManager(IOptions<MQConfig> options)
+        {
+            _options = options;
+        }
+
+        public bool CreateFile()
+        {
+            var input = _options.Value.FilesRoute + _options.Value.InputData;
+            var output = _options.Value.FilesRoute + _options.Value.OutputData;
+            try
+            {
+                File.Create(input).Dispose();
+                File.Create(output).Dispose();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DelteFiles()
+        {
+            var files = Directory.GetFiles(_options.Value.FilesRoute);
+            try
+            {
+                foreach (var file in files)
+                {
+                    File.Delete(file);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Api.Services/MQBrokerFile.cs
+++ Api.Services/MQBrokerFile.cs
@@ -24,37 +24,5 @@ namespace Api.Services
         {
             throw new NotImplementedException();
         }
-
-        public void CreateFiles()
-        {
-            var input = _options.Value.FilesRoute + _options.Value.InputData;
-            var output = _options.Value.FilesRoute + _options.Value.OutputData;
-            try
-            {
-                File.Create(input).Dispose();
-                File.Create(output).Dispose();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
-
-        public void DeleteFiles()
-        {
-            var files = Directory.GetFiles(_options.Value.FilesRoute);
-            try
-            {
-                foreach(var file in files)
-                {
-                    File.Delete(file);
-                }
-            }
-            catch(Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
